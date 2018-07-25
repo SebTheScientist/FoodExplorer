37c2aa6a7d7681fe0d72fb24d74cfb60b1039256
SebTheScientist <sebastian.scharf@live.de>
2018-07-25 23:32:36 +0200

    added one recipe functionality

--- app.R
+++ app.R
@@ -1,12 +1,14 @@
 ## app.R ##
 library(shinydashboard)
 library(DT)
+require(googlesheets)
+require(tidyverse)
+
 
 # pre-load recipes
 recipes1 <- gs_key("15HBB5cF983vW9hTPaR-eE46gfDv94Qax8gmQxCxmZp0")
 
-currentRecipe <- recipes1 %>%
-  gs_read(ws = "Spaghetti Carbonara")
+
 
 ui <- dashboardPage(
   dashboardHeader(title = "Food Explorer"),
@@ -16,39 +18,47 @@ ui <- dashboardPage(
     fluidRow(
       h1("Spaghetti Carbonara"),
       box(width = 4,
-          #DT::dataTableOutput("ingredientTable")
+          title = "Ingredients",
           tableOutput("ingredientTable")
           ),
       
-      box(
-        title = "Controls",
-        sliderInput("slider", "Number of observations:", 1, 100, 50),
-        verbatimTextOutput("ingredientList")
+      box(width = 8,
+        title = "Preparation",
+        uiOutput("steps")
       )
     )
-  )
+  ),
+  
+  tags$head(tags$style("#steps{font-size: 20px;}"))
 )
 
 server <- function(input, output) {
-  set.seed(122)
-  histdata <- rnorm(500)
   
-  output$plot1 <- renderPlot({
-    data <- histdata[seq_len(input$slider)]
-    hist(data)
+  reactiveRecipe <- reactive({
+    recipes1 %>%
+      gs_read(ws = "Spaghetti Carbonara")
+  })
+
+  output$ingredientTable <- renderTable({
+    currentRecipe <- reactiveRecipe()
+    currentRecipe <- subset(currentRecipe, currentRecipe$type == "ingredient")
+    currentRecipe[c("amount", "unit", "name")]
+  }, colnames = F, align = "rll", rownames = F, na = "", width = "auto")
+  
+  reactiveSteps <- reactive({
+    frontmatter <- '<ol type="1">'
+    currentRecipe <- reactiveRecipe()
+    currentRecipe <- subset(currentRecipe, currentRecipe$type == "step")
+    for (i in 1:length(currentRecipe$type)) {
+      snippet <- paste0("<li>", currentRecipe$name[i], "</li>")
+      frontmatter <- paste0(frontmatter, snippet)
+    }
+    frontmatter
   })
   
-  output$ingredientList  <- renderText({
-    paste(currentRecipe)
-  })
-  
-  # output$ingredientTable = DT::renderDataTable({
-  #   currentRecipe
-  # })
-  
-  output$ingredientTable = renderTable({
-    currentRecipe
-  })
+  output$steps <- renderUI(
+    HTML(reactiveSteps())
+  )
   
 }
 
